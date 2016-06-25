06114246f10c9d58e4e1f33f89b50609597ac4d9
xles <xles@mirakulix.org>
2016-06-25 02:18:52 +0200

    Cleaned up gets and getch.

--- js/stdio.js
+++ js/stdio.js
@@ -1,24 +1,21 @@
-var gets = function(_prompt) {
+function gets(_prompt) {
 	if (_prompt)
 		prompt(_prompt);
 	return new Promise(function(resolve, reject) {
 		document.addEventListener('gets', function (e) {
 			console.log('string acquired: %O', e.detail);
-			//document.dispatchEvent(activityEvent);
 			resolve(e.detail);
 		}, false);
 	});
-};
+}
 
-var getch = function(_prompt) {
+function getch(_prompt) {
 	if (_prompt)
 		prompt(_prompt);
 	return new Promise(function(resolve, reject) {
 		document.addEventListener('getch', function (e) {
 			console.log('character acquired: %O', e.detail);
-			//document.dispatchEvent(activityEvent);
 			resolve(e.detail);
 		}, false);
 	});
-};
-
+}
